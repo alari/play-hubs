5e9e7e55f3d89e5fa55e56269772b12313215b39
Dmitry Kurinskiy <name.alari@gmail.com>
2013-12-24 16:20:11 +0400

    compile bugs fixed

--- module-code/app/mirari/hubs/http/HttpTopic.scala
+++ module-code/app/mirari/hubs/http/HttpTopic.scala
@@ -14,10 +14,10 @@ trait HttpTopic[T] extends HubTopic[T]{
   val handleHttpAction: PartialFunction[HttpAction[State],Unit]
 
   val httpBehaviour: Receive = {
-    case ha@HttpAction(_, s: State, _) =>
-      handleHttpAction.applyOrElse(ha.asInstanceOf[HttpAction[State]], _ => sender ! Unwished.NotFound)
+    case ha@HttpAction(_, s, _) if s.isInstanceOf[State] =>
+      handleHttpAction.applyOrElse(ha.asInstanceOf[HttpAction[State]], {_: HttpAction[_] => sender ! Unwished.NotFound})
 
-    case _: HttpAction =>
+    case _: HttpAction[_] =>
       sender ! Unwished.NotFound
   }
 }
