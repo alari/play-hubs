fe91afcb8799065e639c52549e3543f5860e0715
Dmitry Kurinskiy <name.alari@gmail.com>
2013-12-24 16:09:38 +0400

    http topic

---
+++ module-code/app/mirari/hubs/http/HttpTopic.scala
@@ -0,0 +1,23 @@
+package mirari.hubs.http
+
+import mirari.hubs.HubTopic
+import akka.actor.Actor
+import mirari.wished.Unwished
+
+/**
+ * @author alari
+ * @since 12/24/13
+ */
+trait HttpTopic[T] extends HubTopic[T]{
+  topic: Actor =>
+
+  val handleHttpAction: PartialFunction[HttpAction[State],Unit] = {case _ if false => }
+
+  val httpBehaviour: Receive = {
+    case ha@HttpAction(_, s: State, _) =>
+      handleHttpAction.applyOrElse(ha.asInstanceOf[HttpAction[State]], _ => sender ! Unwished.NotFound)
+
+    case _: HttpAction =>
+      sender ! Unwished.NotFound
+  }
+}
