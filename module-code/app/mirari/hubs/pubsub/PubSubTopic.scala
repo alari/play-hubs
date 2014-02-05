6d4eadd377dad9421928b2ba1a8be5a101baa914
Dmitry Kurinskiy <name.alari@gmail.com>
2014-02-05 16:28:52 +0400

    tiny changes

--- module-code/app/mirari/hubs/pubsub/PubSubTopic.scala
+++ module-code/app/mirari/hubs/pubsub/PubSubTopic.scala
@@ -3,82 +3,85 @@ package mirari.hubs.pubsub
 import akka.actor._
 import scala.concurrent.duration.FiniteDuration
 import scala.Some
-import mirari.hubs.{Hubs, HubTopic}
+import mirari.hubs.HubTopic
 
 /**
-  * @author alari
-  * @since 12/19/13
-  */
-trait PubSubTopic[T] extends HubTopic[T]{
-   topic: Actor =>
+ * @author alari
+ * @since 12/19/13
+ */
+trait PubSubTopic[T] extends HubTopic[T] {
+  topic: Actor =>
 
-   private var listeners = Set[ActorRef]()
-   private var timeout: Option[Cancellable] = None
+  private var listeners = Set[ActorRef]()
+  private var timeout: Option[Cancellable] = None
 
-   def timeoutDelay = FiniteDuration(10, "seconds")
+  def timeoutDelay = FiniteDuration(10, "seconds")
+
   /**
    * Should actor die when there's no listeners connected or not
    */
-   val timeoutsEnabled = true
+  val timeoutsEnabled = true
 
-   protected def actualListeners = listeners
+  protected def actualListeners = listeners
 
   /**
    * Clears or launches timeout
    */
-   def checkTimeout() {
-     if (listeners.isEmpty) launchTimeout()
-     else clearTimeout()
-   }
+  def checkTimeout() {
+    if (listeners.isEmpty) launchTimeout()
+    else clearTimeout()
+  }
 
-   def clearTimeout() {
-     timeout.map(_.cancel())
-     timeout = None
-   }
+  def clearTimeout() {
+    timeout.map(_.cancel())
+    timeout = None
+  }
 
-   def launchTimeout() {
-     import context.dispatcher
-     if (timeout.isEmpty && timeoutsEnabled) timeout = Some(context.system.scheduler.scheduleOnce(timeoutDelay, self, PoisonPill))
-   }
+  def launchTimeout() {
+    import context.dispatcher
+    if (timeout.isEmpty && timeoutsEnabled) timeout = Some(context.system.scheduler.scheduleOnce(timeoutDelay, self, PoisonPill))
+  }
 
-   launchTimeout()
+  launchTimeout()
 
-   def join(actor: ActorRef) {
-     listeners += actor
-     context.watch(actor)
-     clearTimeout()
-   }
+  def join(actor: ActorRef) {
+    listeners += actor
+    context.watch(actor)
+    clearTimeout()
+  }
 
-   def leave(actor: ActorRef) {
-     listeners -= actor
-     context.unwatch(actor)
-     checkTimeout()
-   }
+  def leave(actor: ActorRef) {
+    listeners -= actor
+    context.unwatch(actor)
+    checkTimeout()
+  }
 
-   def broadcast(message: Any) {
-     listeners.foreach(_ ! message)
-   }
+  def broadcast(message: Any) {
+    listeners.foreach(_ ! message)
+  }
 
-  type CanSubscribe = PartialFunction[T,Boolean]
+  type CanSubscribe = PartialFunction[T, Boolean]
 
-   val canSubscribe: CanSubscribe
+  val canSubscribe: CanSubscribe
 
   abstract override def topicBehaviour: Receive = super.topicBehaviour orElse pubSubBehaviour
 
   val pubSubBehaviour: Receive = {
-     case Terminated(a) if listeners.contains(a) =>
-       leave(a)
+    case Terminated(a) if listeners.contains(a) =>
+      leave(a)
 
-     case PubSubTopic.Broadcast(m) =>
-       broadcast(m)
+    case PubSubTopic.Broadcast(m) =>
+      broadcast(m)
 
-     case PubSubTopic.Join(state: T) if canSubscribe.applyOrElse(state, {_: T => false: Boolean}) =>
-       join(sender)
+    case PubSubTopic.Join(state: T) if canSubscribe.applyOrElse(state, {
+      _: T => false: Boolean
+    }) =>
+      join(sender)
 
-     case PubSubTopic.Leave if listeners.contains(sender) =>
-       leave(sender)
-   }
- }
+    case PubSubTopic.Leave if listeners.contains(sender) =>
+      leave(sender)
+  }
+}
 
 object PubSubTopic {
 
